240dbc85950955af520ef03a8e9f220908a6e028
Tim Lochmüller <tim@fruit-lab.de>
2020-11-15 21:26:36 +0100

    Add new page properties

--- Classes/Domain/Model/Page.php
+++ Classes/Domain/Model/Page.php
@@ -15,6 +15,36 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
      */
     protected $title = '';
 
+    /**
+     * @var string
+     */
+    protected $subtitle = '';
+
+    /**
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * @var string
+     */
+    protected $abstract = '';
+
+    /**
+     * @var string
+     */
+    protected $author = '';
+
+    /**
+     * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\FileReference>
+     */
+    protected $media;
+
+    public function __construct()
+    {
+        $this->media = new \TYPO3\CMS\Extbase\Persistence\ObjectStorage();
+    }
+
     public function getTitle(): string
     {
         return $this->title;
@@ -23,5 +53,54 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
     public function setTitle(string $title): void
     {
         $this->title = $title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getSubtitle(): string
+    {
+        return $this->subtitle;
+    }
+
+    public function setSubtitle(string $subtitle): void
+    {
+        $this->subtitle = $subtitle;
+    }
+
+    public function getAbstract(): string
+    {
+        return $this->abstract;
+    }
+
+    public function setAbstract(string $abstract): void
+    {
+        $this->abstract = $abstract;
+    }
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): void
+    {
+        $this->author = $author;
+    }
+
+    public function getMedia(): \TYPO3\CMS\Extbase\Persistence\ObjectStorage
+    {
+        return $this->media;
+    }
+
+    public function setMedia(\TYPO3\CMS\Extbase\Persistence\ObjectStorage $media): void
+    {
+        $this->media = $media;
     }
 }
