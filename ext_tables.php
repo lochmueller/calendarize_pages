534dff8ec8a01ef8d268fa28404d0de917ef6248
Fabian Schiller <schiller.fabian25@gmail.com>
2024-12-17 14:06:34 +0100

    [TASK] Move user.tsconfig to own file

--- ext_tables.php
+++ ext_tables.php
@@ -6,7 +6,3 @@ $GLOBALS['PAGES_TYPES'][132] = [
     'type' => 'web',
     'allowedTables' => '*',
 ];
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-    options.pageTree.doktypesToShowInNewPageDragArea := addToList(132)
-');
