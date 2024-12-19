54a5057e361c26bc8d856e9faff3aae68f0e513c
Tim Lochmüller <tim@fruit-lab.de>
2024-12-19 15:39:54 +0100

    Still support TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'tim@fruit-lab.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.5.99',
+            'typo3' => '12.4.0-13.4.99',
             'php' => '8.1.0-8.3.99',
             'calendarize' => '10.0.0-13.99.99',
         ],
--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,16 @@
 <?php
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+
 defined('TYPO3') || die();
 
 $GLOBALS['PAGES_TYPES'][132] = [
     'type' => 'web',
     'allowedTables' => '*',
 ];
+
+if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() <= 12) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+        @import \'EXT:calendarize_pages/Configuration/user.tsconfig\'
+    ');
+}
