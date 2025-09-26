122ec3eb63be87c60cb307e04445119ea2e318b2
Tim Lochmüller <tim.lochmueller@hdnet.de>
2025-09-26 09:43:06 +0200

    Fix #16 - change dependencies in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,9 +13,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'tim@fruit-lab.de',
     'constraints' => [
         'depends' => [
+            'php' => '8.1.0-8.4.99',
             'typo3' => '12.4.0-13.4.99',
-            'php' => '8.1.0-8.3.99',
-            'calendarize' => '10.0.0-13.99.99',
+            'calendarize' => '13.0.0-14.99.99',
         ],
     ],
     'autoload' => [
